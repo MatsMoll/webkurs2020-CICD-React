1eaefd6cc55b5f2f9276ddfffea2f4a2d4d76854
Mats Eikeland Mollestad <mem@mollestad.no>
2020-11-02 17:38:24 +0100

    Updates

---
+++ precache-manifest.8ed262e3625a9f3b115185310a45a99f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb675126b712b00335f87f4c67a39fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5578f31df6161dd6b32d",
+    "url": "/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "2abbfad30cef38b7d94a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5578f31df6161dd6b32d",
+    "url": "/static/js/2.1225ef53.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1225ef53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abbfad30cef38b7d94a",
+    "url": "/static/js/main.681fcf34.chunk.js"
+  },
+  {
+    "revision": "f233014d846008652b8c",
+    "url": "/static/js/runtime-main.5ff388ac.js"
+  }
+]);
